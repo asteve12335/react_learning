7a92296abefc7740896a062ef98761dd59a63d63
asteve12335 <asteve12335@gmail.com>
2025-09-01 22:57:34 +0100

    Adjustment of frontend Structure

---
+++ react-app/src/components/layout/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <nav className="bg-blue-600 text-white px-6 py-3 shadow-md">
+            <h1 className="text-xl font-bold">CivicLens</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
